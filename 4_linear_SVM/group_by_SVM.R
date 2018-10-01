3498721eb9977e7782e2ff90a623adf001ae79a3
FangYo <fy6vj@virginia.edu>
2018-10-01 13:54:12 -0400

    sklearn: linear SVM. MAE: 4.32720

---
+++ 4_linear_SVM/group_by_SVM.R
@@ -0,0 +1,10 @@
+# sys6018-competition-titanic
+# https://www.kaggle.com/c/titanic/overview
+# Fang You (fy6vj)
+
+library(readr)
+library(dplyr)
+
+py_output <- read_csv("test_sklearn_SVM_output.csv")
+output_clean <- py_output %>% group_by(user.id) %>% summarise(Mean = mean(age))
+write.table(output_clean, file = "blog_sklearn_SVM.csv", row.names=F, col.names=c("user.id", "age"), sep=",")
