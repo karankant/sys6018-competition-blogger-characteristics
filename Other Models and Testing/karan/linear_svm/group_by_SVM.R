8df79864ceccdc870336c7d3e5313cee02148c52
karankant <karan.kant2@gmail.com>
2018-10-10 23:55:22 -0400

    update

--- Other Models and Testing/karan/linear_svm/group_by_SVM.R
+++ Other Models and Testing/karan/linear_svm/group_by_SVM.R
@@ -5,6 +5,6 @@
 library(readr)
 library(dplyr)
 
-py_output <- read_csv("test_sklearn_SVM_CV_output.csv")
+py_output <- read_csv("test_sklearn_SVM_output.csv")
 output_clean <- py_output %>% group_by(user.id) %>% summarise(Mean = mean(age))
-write.table(output_clean, file = "blog_sklearn_SVM_CV.csv", row.names=F, col.names=c("user.id", "age"), sep=",")
+write.table(output_clean, file = "blog_sklearn_SVM2.csv", row.names=F, col.names=c("user.id", "age"), sep=",")
